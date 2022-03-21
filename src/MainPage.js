be483de30bdb6394ec91fda93adee09bbf96fa2a
FirminEx <firminexertier1@gmail.com>
2022-03-21 14:37:40 +0100

    deleted useless files

---
+++ src/MainPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {connected: false};
+    }
+
+    render() {
+        return (
+            <div id='mainpage'>
+                slt
+            </div>
+        );
+    }
+}
+
+
+export default MainPage;
