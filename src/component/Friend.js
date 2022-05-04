fab31389fddc84a7207f2372ce2783519c6cc10e
FirminEx <firminexertier1@gmail.com>
2022-05-04 15:56:54 +0200

    get conversation front end

--- src/component/Friend.js
+++ src/component/Friend.js
@@ -1,11 +1,20 @@
 import React from "react";
 import placeholder from "../img/placeholder.jpg"
 import {imageToBase64} from "../data_process/image";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {displaySlice} from "../redux/features/displaySlice"
+import {fetchConversation} from "../redux/features/conversationSlice";
 
 function Friend(props) {
     const dispatch = useDispatch()
+    const {user} = useSelector(state => state.logIn)
+    const friendConversation = (e) => {
+        e.preventDefault()
+        dispatch(fetchConversation({friendID: props.user._id, userID: user._id}))
+        dispatch(displaySlice.actions.goToConversation())
+    }
+
+
     return(
     <div class="friend">
         {props.user.picture ?
@@ -13,7 +22,7 @@ function Friend(props) {
             : <img src={placeholder} alt={props.user.userName} className="friendpp"/>
         }
         <div class="friendname">{props.user.userName}</div>
-        <button onClick={() => dispatch(displaySlice.actions.goToConversation())}>Message</button>
+        <button onClick={friendConversation}>Message</button>
     </div>
 );
 }
--- src/redux/features/conversationSlice.js
+++ src/redux/features/conversationSlice.js
@@ -1,17 +1,53 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     friend: '',
     loading: false,
     conversation: null,
     messageList: [],
+    error: ''
 }
+
+const url = 'http://localhost:8000/conversation/'
+
+export const fetchConversation = createAsyncThunk(
+    'conversation/fetchConversation',
+    async (parameters ) => {
+        const {friendID, userID} = parameters;
+        if(!userID) return Promise.reject(new Error('Not connected'))
+        console.log(userID)
+        const response = await axios.post(url + 'getConversation', {user1: friendID, user2: userID})
+        if(!response) return Promise.reject(new Error(response.data))
+        return response.data
+    }
+)
+
+//TODO convert conversation into message list + friend name
 
 export const conversationSlice = createSlice({
     name: 'conversation',
     initialState,
     reducers: {
         resetConversation: () => initialState
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchConversation.pending, (state) => {
+                state.loading = true
+                state.friend = ''
+                state.conversation = null
+                state.messageList = []
+                state.error = ''
+            })
+            .addCase(fetchConversation.fulfilled, (state, action) => {
+                state.loading = false
+                state.conversation = action.payload
+            })
+            .addCase(fetchConversation.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.error.message
+            })
     }
 })
 
